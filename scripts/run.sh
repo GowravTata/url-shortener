97e5663be077c37289d161f8ccfdc7d7ddd2c1a9
GowravTata <gowravsaitata@gmail.com>
2026-06-30 14:15:51 +0000

    Made Changes

--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-find . -type d -name "__pycache__" -not -path "./venv/*" -exec rm -rf {} +
-
-fastapi dev app/main.py
--- scripts/user_data_debug.sh
+++
@@ -1,3 +0,0 @@
-sudo cloud-init status --long
-sudo cat /var/lib/cloud/instance/user-data.txt
-sudo tail -300 /var/log/cloud-init-output.log
